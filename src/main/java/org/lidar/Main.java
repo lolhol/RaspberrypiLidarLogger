094e232e03d4cfd1707b2e6c4ab60d20624c6db9
brigerodev <62488289+lolhol@users.noreply.github.com>
2024-03-02 14:40:26 -0800

    build.gradle update

--- src/main/java/org/lidar/Main.java
+++ src/main/java/org/lidar/Main.java
@@ -8,20 +8,22 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         // Specify the GPIO pin number
-        int pinNumber = 2; // Replace with your desired GPIO pin number
-        int pinNumberRec = 4;
+        int pinNumber = 26; // Replace with your desired GPIO pin number
+        // gpio-406
+        int pinNumberRec = 24;
+        // gpio-407
 
         // Export the pin
         exportPin(pinNumber);
 
         // Set the direction of the pin (in this case, as an output)
-        setDirection(pinNumber, "out");
+        setDirection("gpio-406", "out");
 
         // Control the state of the pin (turn it on and off)
-        controlPin(pinNumber, 1); // 1 for HIGH, 0 for LOW
+        controlPin("gpio-406", 1); // 1 for HIGH, 0 for LOW
 
         while (true) {
-            int pinValue = readPin(pinNumberRec);
+            int pinValue = readPin("gpio-407");
             System.out.println("Pin " + pinNumber + " state: " + pinValue);
 
             // Sleep for a short duration before reading again
@@ -31,7 +33,7 @@ public class Main {
                 e.printStackTrace();
             }
 
-            controlPin(pinNumber, Math.random() > 0.5 ? 1 : 0);
+            controlPin("gpio-407", Math.random() > 0.5 ? 1 : 0);
         }
     }
 
@@ -43,15 +45,15 @@ public class Main {
         }
     }
 
-    private static void setDirection(int pinNumber, String direction) {
-        try (FileWriter writer = new FileWriter("/sys/class/gpio/gpio" + pinNumber + "/direction")) {
+    private static void setDirection(String pin, String direction) {
+        try (FileWriter writer = new FileWriter("/sys/class/gpio/" + pin + "/direction")) {
             writer.write(direction);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static void controlPin(int pinNumber, int value) {
+    private static void controlPin(String pinNumber, int value) {
         try (FileWriter writer = new FileWriter("/sys/class/gpio/gpio" + pinNumber + "/value")) {
             writer.write(Integer.toString(value));
         } catch (IOException e) {
@@ -67,7 +69,7 @@ public class Main {
         }
     }
 
-    private static int readPin(int pinNumber) {
+    private static int readPin(String pinNumber) {
         try (BufferedReader reader = new BufferedReader(new FileReader("/sys/class/gpio/gpio" + pinNumber + "/value"))) {
             String valueStr = reader.readLine();
             return Integer.parseInt(valueStr.trim());
