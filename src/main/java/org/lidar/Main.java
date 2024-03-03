5dc3ed382d36b2a3b0a09dac741260380feff2e1
brigerodev <62488289+lolhol@users.noreply.github.com>
2024-03-02 17:01:06 -0800

    build.gradle update

--- src/main/java/org/lidar/Main.java
+++ src/main/java/org/lidar/Main.java
@@ -1,19 +1,28 @@
 package org.lidar;
 
 import com.pi4j.io.gpio.*;
+import com.pi4j.io.gpio.event.*;
 
 public class Main {
     public static void main(String[] args) {
         final GpioController gpio = GpioFactory.getInstance();
-        GpioFactory.setDefaultProvider(new RaspiGpioProvider(RaspiPinNumberingScheme.DEFAULT_PIN_NUMBERING));
-        final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02, "MyLED", PinState.LOW);
-        pin.high();
+
+        // Provision GPIO pin #17 as an output pin and turn it off
+        final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_17, PinState.LOW);
+
+        // Add a listener to detect state changes on the pin
+        pin.addListener((GpioPinListenerDigital) event -> {
+            System.out.println("Pin state changed: " + event.getState());
+        });
+
+        // Toggle the pin state every second
         try {
-            Thread.sleep(5000);
+            while (true) {
+                pin.toggle();
+                Thread.sleep(1000);
+            }
         } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
-        pin.low();
-        gpio.shutdown();
     }
 }
